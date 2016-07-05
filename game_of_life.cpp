8140b36c372c1b475efd7ad1713f845ab550aaf3
ZweiDrittel <veit.rautmann@web.de>
2016-07-05 08:43:13 +0200

    Description in game_of_life.cpp
    Just added a short description to the file

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,1 +1,7 @@
+//------------------------------------------------
+//-------------Game Of Life-----------------------
+//------------------------------------------------
+//Simulate the circle of life for a world full of rabbits
+//You can choose the domainsize and the initial state
 
+#include <iostream>
